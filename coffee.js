38253b2ec673e8123429e104fabf930f329567e0
Victor-G87 <victor.g.hernandez87@gmail.com>
2021-09-05 20:59:24 -0500

    fixed coffeproject

---
+++ coffee.js
@@ -0,0 +1,126 @@
+"use strict";
+
+function renderCoffee(coffee) {
+    var html = '<div class="coffee">';
+    // html += '<td>' + coffee.id + '</td>';
+    html += '<div class="d-flex align-items-baseline">';
+    html += '<h3>' + coffee.name + '</h3>';
+    html += '<p class="ml-2 text-secondary">' + coffee.roast + '</p>';
+    html += '</div>';
+    html += '</div>';
+
+    return html;
+}
+
+function renderCoffees(coffees) {
+    var html = '';
+    for(var i = 0; i < coffees.length; i++) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
+
+
+
+function updateCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value;
+    var coffeNameInput = coffeeName.value.toLowerCase();
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast && coffee.name.toLowerCase().indexOf(coffeNameInput) !== -1) {
+            filteredCoffees.push(coffee);
+        }else if(selectedRoast === "All" && coffee.name.toLowerCase().indexOf(coffeNameInput) !== -1) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+
+
+
+
+function addCoffee(e) {
+    e.preventDefault();
+    coffees.push({
+        name: coffeeAddName.value,
+        roast: roastAddSelection.value
+    });
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        filteredCoffees.push(coffee)
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+
+}
+
+
+
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+
+coffees.reverse();
+
+var tbody = document.querySelector('#coffees');
+var submitButton = document.querySelector('#submit');
+var submitNewCoffeeButton = document.querySelector('#add-submit');
+var roastSelection = document.querySelector('#roast-selection');
+var roastAddSelection = document.querySelector('#addRoast-selection');
+var coffeeName =  document.querySelector('#coffee-name');
+var coffeeAddName =  document.querySelector('#add-coffeeName');
+
+tbody.innerHTML = renderCoffees(coffees);
+
+submitNewCoffeeButton.addEventListener('click', addCoffee);
+
+submitButton.addEventListener('click', updateCoffees);
+
+roastSelection.addEventListener('change', updateCoffees);
+
+coffeeName.addEventListener('keyup', updateCoffees);
+
+
+
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+    if(this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+})
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
+
+
+
+document.getElementById('startAnim').addEventListener('click', function() {
+    document.getElementsByClassName('my-main')[0].classList.add('pulsate-bck');
+});
+
+document.getElementById('stopAnim').addEventListener('click', function() {
+    document.getElementsByClassName('my-main')[0].classList.remove('pulsate-bck');
+});
